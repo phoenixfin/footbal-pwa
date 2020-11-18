baad2fc9a00acd3ab0d531e2e7bc068798f1bf4b
Aditya Firman Ihsan <aditya.fphoenix@gmail.com>
2020-11-19 05:41:55 +0700

    Change service-worker framework to workbox

---
+++ archive/service-worker-old.js
@@ -0,0 +1,89 @@
+const CACHE_NAME = "football-v1";
+const urlsToCache = [
+    "/",
+    "/nav.html",
+    "/index.html",
+    "/team.html",
+    "/pages/home.html",
+    "/pages/about.html",
+    "/pages/teams.html",
+    "/pages/matches.html",
+    "/pages/saved.html",
+    "/css/materialize.min.css",
+    "/js/materialize.min.js",
+    "/manifest.json",
+    "/js/nav.js",
+    "/js/init.js",
+    "/js/api.js",
+    "/js/db.js",
+    "/js/idb.js",
+    "/js/push.js",
+    "/icon.png",
+    "https://fonts.googleapis.com/icon?family=Material+Icons",
+    "https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2"
+];
+
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", function (event) {
+    const base_url = "https://api.football-data.org/v2/";
+    if (event.request.url.indexOf(base_url) > -1) {
+        event.respondWith(
+            caches.open(CACHE_NAME).then(function (cache) {
+                return fetch(event.request).then(function (response) {
+                    cache.put(event.request.url, response.clone());
+                    return response;
+                })
+            })
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request, {cacheName:CACHE_NAME, ignoreSearch: true}).then(function (response) {
+                return response || fetch(event.request);
+            })
+        )
+    }
+});
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName != CACHE_NAME) {
+                        console.log("ServiceWorker: cache " + cacheName + " dihapus");
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('push', function(event) {
+    var body;
+    if (event.data) {
+        body = event.data.text();
+    } else {
+        body = 'Push message no payload';
+    }
+    var options = {
+        body: body,
+        icon: '/icon.png',
+        badge: '/icon.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+        }
+    };
+    event.waitUntil(
+        self.registration.showNotification('Push Notification', options)
+    );
+});
